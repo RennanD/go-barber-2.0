b3e338187d32cfb4d58d3a952b46c07e97a1591a
RennanD <rplaykl@gmail.com>
2020-04-14 15:10:40 -0300

    feat: backend - Create relationship between appointments and users

--- backend/src/database/migrations/1586803571503-CreateAppointments.ts
+++ backend/src/database/migrations/1586803571503-CreateAppointments.ts
@@ -9,7 +9,7 @@ export default class CreateAppointments1586803571503
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
