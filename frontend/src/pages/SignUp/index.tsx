8e7769bcca7b4a4f411985096e394664a19052e3
RennanD <rplaykl@gmail.com>
2020-05-07 12:16:04 -0300

    feat: frontend - Styled SignUp page

---
+++ frontend/src/pages/SignUp/index.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi';
+
+import { Container, Content, Background } from './styles';
+
+import logoImg from '../../assets/logo.svg';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+const SignUp: React.FC = () => (
+  <Container>
+    <Background />
+
+    <Content>
+      <img src={logoImg} alt="GoBarber" />
+
+      <form>
+        <h1>Faça seu cadastro</h1>
+
+        <Input name="name" icon={FiUser} type="text" placeholder="Nome nome" />
+        <Input name="email" icon={FiMail} type="text" placeholder="E-mail" />
+        <Input
+          name="password"
+          type="password"
+          icon={FiLock}
+          placeholder="Senha"
+        />
+
+        <Button type="submit">Cadastrar</Button>
+      </form>
+
+      <a href="/forgot">
+        <FiArrowLeft />
+        Voltar para logon
+      </a>
+    </Content>
+  </Container>
+);
+
+export default SignUp;
