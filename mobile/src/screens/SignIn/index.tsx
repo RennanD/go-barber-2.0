709eca1be653433b6e22bb760bd3805d72515d44
RennanD <rplaykl@gmail.com>
2020-07-23 00:06:22 -0300

    mobile - feat: Styled SignUp Screen

--- mobile/src/screens/SignIn/index.tsx
+++ mobile/src/screens/SignIn/index.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   Image,
   KeyboardAvoidingView,
   Platform,
   View,
   ScrollView,
+  Keyboard,
 } from 'react-native';
+
+import { useNavigation } from '@react-navigation/native';
 
 import Icon from 'react-native-vector-icons/Feather';
 
@@ -23,41 +26,63 @@ import Button from '../../components/Button';
 
 import logoImage from '../../assets/images/logo.png';
 
-const SignIn: React.FC = () => (
-  <>
-    <KeyboardAvoidingView
-      style={{ flex: 1 }}
-      enabled
-      behavior={Platform.OS === 'ios' ? 'padding' : undefined}
-    >
-      <ScrollView
-        contentContainerStyle={{
-          flex: 1,
-        }}
-        keyboardShouldPersistTaps="handled"
+const SignIn: React.FC = () => {
+  const { navigate } = useNavigation();
+
+  const [showKeyboard, setShowKeyboard] = useState(false);
+
+  useEffect(() => {
+    Keyboard.addListener('keyboardDidShow', () => setShowKeyboard(true));
+    Keyboard.addListener('keyboardDidHide', () => setShowKeyboard(false));
+
+    return () => {
+      Keyboard.removeListener('keyboardDidShow', () => setShowKeyboard(true));
+      Keyboard.removeListener('keyboardDidHide', () => setShowKeyboard(false));
+    };
+  }, []);
+
+  return (
+    <>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        enabled
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
       >
-        <Container>
-          <Image source={logoImage} />
-          <View>
-            <Title>Faça seu logon</Title>
-          </View>
+        <ScrollView
+          contentContainerStyle={{
+            flex: 1,
+          }}
+          keyboardShouldPersistTaps="handled"
+        >
+          <Container>
+            <Image source={logoImage} />
+            <View>
+              <Title>Faça seu logon</Title>
+            </View>
 
-          <Input name="email" icon="mail" placeholder="E-mail" />
-          <Input name="password" icon="lock" placeholder="Senha" />
-          <Button>Entrar</Button>
+            <Input name="email" icon="mail" placeholder="E-mail" />
+            <Input name="password" icon="lock" placeholder="Senha" />
+            <Button>Entrar</Button>
 
-          <ForgotPassword>
-            <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
-          </ForgotPassword>
-        </Container>
-      </ScrollView>
-    </KeyboardAvoidingView>
+            <ForgotPassword>
+              <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
+            </ForgotPassword>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
 
-    <CreateAccountButton>
-      <Icon name="log-in" color="#ff9000" size={20} />
-      <CreateAccountButtonText>Criar uma conta</CreateAccountButtonText>
-    </CreateAccountButton>
-  </>
-);
+      {!showKeyboard && (
+        <CreateAccountButton
+          onPress={() => {
+            navigate('SignUp');
+          }}
+        >
+          <Icon name="log-in" color="#ff9000" size={20} />
+          <CreateAccountButtonText>Criar uma conta</CreateAccountButtonText>
+        </CreateAccountButton>
+      )}
+    </>
+  );
+};
 
 export default SignIn;
--- mobile/src/screens/SignUp/index.tsx
+++ mobile/src/screens/SignUp/index.tsx
@@ -1,8 +1,74 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import {
+  Image,
+  KeyboardAvoidingView,
+  Platform,
+  View,
+  ScrollView,
+  Keyboard,
+} from 'react-native';
 
-// import { Container } from './styles';
+import { useNavigation } from '@react-navigation/native';
 
-const SignUp: React.FC = () => <View />;
+import Icon from 'react-native-vector-icons/Feather';
+
+import { Container, Title, BackToSignIn, BackToSignInText } from './styles';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import logoImage from '../../assets/images/logo.png';
+
+const SignUp: React.FC = () => {
+  const { goBack } = useNavigation();
+  const [showKeyboard, setShowKeyboard] = useState(false);
+
+  useEffect(() => {
+    Keyboard.addListener('keyboardDidShow', () => setShowKeyboard(true));
+    Keyboard.addListener('keyboardDidHide', () => setShowKeyboard(false));
+
+    return () => {
+      Keyboard.removeListener('keyboardDidShow', () => setShowKeyboard(true));
+      Keyboard.removeListener('keyboardDidHide', () => setShowKeyboard(false));
+    };
+  }, []);
+
+  return (
+    <>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        enabled
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+      >
+        <ScrollView
+          contentContainerStyle={{
+            flex: 1,
+          }}
+          keyboardShouldPersistTaps="handled"
+        >
+          <Container>
+            <Image source={logoImage} />
+            <View>
+              <Title>Cire ua conta</Title>
+            </View>
+
+            <Input name="name" icon="user" placeholder="Senha" />
+
+            <Input name="email" icon="mail" placeholder="E-mail" />
+            <Input name="password" icon="lock" placeholder="Senha" />
+            <Button>Entrar</Button>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+
+      {!showKeyboard && (
+        <BackToSignIn onPress={() => goBack()}>
+          <Icon name="arrow-left" color="#fff" size={20} />
+          <BackToSignInText>Voltar para logon</BackToSignInText>
+        </BackToSignIn>
+      )}
+    </>
+  );
+};
 
 export default SignUp;
